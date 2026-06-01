7f6e7e6ca1e2b2c2d594f205fd492622a1c5a9f4
Michael Ribeiro <seu-email-do-github@example.com>
2026-06-01 20:36:36 -0300

    fix: Inclui a Coluna de rank na minha entidade, porem ja estava incluida na migration V2

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -19,14 +19,20 @@ public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
+    @Column(name = "nome")
     private String nome;
 
-    @Column(unique = true)
+    @Column(unique = true, name = "email")
     private String email;
 
+    @Column(name = "idade")
     private int idade;
+
+    @Column(name = "rank")
+    private String rank;
 
     // @ManyToOne  - um ninja tem uma única missão
     @ManyToOne
